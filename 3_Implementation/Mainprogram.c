526f1800440981f49940025f905c3a0c106b1a6c
nagaphanendrareddysatti <102752587+nagaphanendrareddysatti@users.noreply.github.com>
2022-04-03 17:01:27 +0530

    Add files via upload

---
+++ 3_Implementation/Mainprogram.c
@@ -0,0 +1,18 @@
+#include"Tictactoe.h"
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include <windows.h>
+
+
+void menu();
+
+
+void main()
+{
+    system("cls");
+    menu();
+    getch();
+
+
+}
